729ea13a143905d89176f3d60bc31495839ecdae
NechaevMichael <nechaevmichael@mail.ru>
2022-10-12 22:58:02 +0500

    correct Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array = {"Russia", "2", "world", ":-)", "-2", "3", "123"};
+﻿string[] array = {"Russia", "2", "world", ":-)", "-2", "3", "12", "Git"};
 
 // int i = 0;
 // while (i < array.Length)
@@ -9,26 +9,60 @@
 
 string[] CreateArray(string[] arr)
 {
-    string[] result = new string[arr.Length];
+    int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length <= 3) result[i] = arr[i];
+        if (arr[i].Length <= 3)
+        {
+            count += 1;
+        }
     }
-    return result;
+    string[] array = new string[count];
+    {
+        int j = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= 3) 
+            {
+                array[j] = arr[i];
+                j += 1;
+            }
+        }
+    }
+    return array;
 }
 
 void PrintArray(string[] arr)
 {
-    int count = arr.Length;
+    Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i == 0) Console.Write("[");
-        if (i == count - 1) Console.Write($"{arr[i]}]");
-        else 
-        {
-            if (arr[i] != default) Console.Write($"{arr[i]}, ");
-        }
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
     }
+    Console.Write("]");
 }
+// {
+//     string[] result = new string[arr.Length];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i].Length <= 3) result[i] = arr[i];
+//     }
+//     return result;
+// }
+
+// void PrintArray(string[] arr)
+// {
+//     int count = arr.Length;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i == 0) Console.Write("[");
+//         if (i == count - 1) Console.Write($"{arr[i]}]");
+//         else 
+//         {
+//             if (arr[i] != default) Console.Write($"{arr[i]}, ");
+//         }
+//     }
+// }
 string[] arrayResult = CreateArray(array);
 PrintArray(arrayResult);
